92211c3ee0db8cea9fccd98b847f1fae7f6f4e02
huahua <17638351213@163.com>
2020-03-06 11:24:50 +0800

    增加连接池配置

--- src/main/java/com/movie/order/entity/Order.java
+++ src/main/java/com/movie/order/entity/Order.java
@@ -1,44 +1,43 @@
 package com.movie.order.entity;
 
-import java.util.Date;
-import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
-@Builder
 public class Order {
     /**
-    * order_id
-    */
+     * order_id
+     */
     private String orderId;
 
     /**
-    * 所属用户编号
-    */
+     * 所属用户编号
+     */
     private Long userId;
 
     /**
-    * 所属场次编号,通过schedule_id-》场次信息-》放映厅信息+电影信息
-    */
+     * 所属场次编号,通过schedule_id-》场次信息-》放映厅信息+电影信息
+     */
     private Long scheduleId;
 
     /**
-    * 电影票座位信息
-    */
+     * 电影票座位信息
+     */
     private String orderPosition;
 
     /**
-    * 订单状态 0：退票中 -1：无法退票  1：已支付 2：退票成功
-    */
+     * 订单状态 0：退票中 -1：无法退票  1：已支付 2：退票成功
+     */
     private Integer orderState;
 
     /**
-    * 订单价格
-    */
+     * 订单价格
+     */
     private Integer orderPrice;
 
     /**
-    * 订单支付时间
-    */
+     * 订单支付时间
+     */
     private Date orderTime;
 }
