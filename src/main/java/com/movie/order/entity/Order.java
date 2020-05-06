57eeefee43ca0c18715205a7562bd5b611c2c159
huahua <17638351213@163.com>
2020-05-06 14:22:00 +0800

    创建订单修改

--- src/main/java/com/movie/order/entity/Order.java
+++ src/main/java/com/movie/order/entity/Order.java
@@ -1,41 +1,41 @@
 package com.movie.order.entity;
 
-import lombok.Data;
-
 import java.util.Date;
+
+import lombok.Data;
 
 @Data
 public class Order {
     /**
      * order_id
      */
-    private String orderId;
-
+    private Long orderId;
+    
     /**
      * 所属用户编号
      */
     private Long userId;
-
+    
     /**
      * 所属场次编号,通过schedule_id-》场次信息-》放映厅信息+电影信息
      */
     private Long scheduleId;
-
+    
     /**
      * 电影票座位信息
      */
     private String orderPosition;
-
+    
     /**
-     * 订单状态 0：退票中 -1：无法退票  1：已支付 2：退票成功
+     * 订单状态 0：退票中 -1：无法退票 1：已支付 2：退票成功
      */
     private Integer orderState;
-
+    
     /**
      * 订单价格
      */
     private Integer orderPrice;
-
+    
     /**
      * 订单支付时间
      */
--- src/main/java/com/movie/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/movie/order/service/impl/OrderServiceImpl.java
@@ -1,74 +1,78 @@
 package com.movie.order.service.impl;
 
-import com.movie.order.dao.OrderMapper;
-import com.movie.order.entity.Order;
-import com.movie.order.service.OrderService;
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
-import java.util.List;
+import com.movie.order.dao.OrderMapper;
+import com.movie.order.entity.Order;
+import com.movie.order.service.OrderService;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class OrderServiceImpl implements OrderService {
-
+    
     @Resource
     private OrderMapper orderMapper;
-
+    
     @Override
     public int deleteByPrimaryKey(String orderId) {
         return orderMapper.deleteByPrimaryKey(orderId);
     }
-
+    
     @Override
     public int insert(Order record) {
         return orderMapper.insert(record);
     }
-
+    
     @Override
     public int insertOrUpdate(Order record) {
         return orderMapper.insertOrUpdate(record);
     }
-
+    
     @Override
     public int insertOrUpdateSelective(Order record) {
         return orderMapper.insertOrUpdateSelective(record);
     }
-
+    
     @Override
-    public int insertSelective(Order record) {
-        return orderMapper.insertSelective(record);
+    public Order insertSelective(Order record) {
+        long orderId = orderMapper.insertSelective(record);
+        record.setOrderId(orderId);
+        return record;
     }
-
+    
     @Override
     public Order selectByPrimaryKey(String orderId) {
         return orderMapper.selectByPrimaryKey(orderId);
     }
-
+    
     @Override
     public int updateByPrimaryKeySelective(Order record) {
         return orderMapper.updateByPrimaryKeySelective(record);
     }
-
+    
     @Override
     public int updateByPrimaryKey(Order record) {
         return orderMapper.updateByPrimaryKey(record);
     }
-
+    
     @Override
     public int updateBatch(List<Order> list) {
         return orderMapper.updateBatch(list);
     }
-
+    
     @Override
     public int updateBatchSelective(List<Order> list) {
         return orderMapper.updateBatchSelective(list);
     }
-
+    
     @Override
     public int batchInsert(List<Order> list) {
         return orderMapper.batchInsert(list);
     }
-
+    
 }
