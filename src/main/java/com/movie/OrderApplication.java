1deb68c48667e40278e22328b0b3febdfe1b1a9e
huahua <17638351213@163.com>
2020-05-06 11:42:43 +0800

    订单服务接口文档

--- src/main/java/com/movie/OrderApplication.java
+++ src/main/java/com/movie/OrderApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableSwagger2Doc
 public class OrderApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
-
+    
 }
